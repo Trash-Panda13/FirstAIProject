30aeac0af960b72c5d821d71261dea66dd98cb38
Panda <theartchristelle@gmail.com>
2026-08-07 21:33:26 +0200

    Hunger is over rated
    Just added the food stuff

--- FirstAIProject.py
+++ FirstAIProject.py
@@ -9,15 +9,24 @@ grid = [
 
 #Creature
 health = 100
+hunger = 100
 energy = 100
 
+
 healthScore = 100 - health
+hungerScore = 100 - hunger
 energyScore = 100 - energy
 
 creaturey = 2
 creaturex = 2
-
 grid [creaturey][creaturex] = "C"
+
+#Food
+food = 20
+
+foody = 1
+foodx = 4
+grid[foody][foodx] = "F"
 
 #The thing idk
 for row in grid:

1f58a7d95922b759205c9d2b1c8afb02aaa3ff11
Panda <theartchristelle@gmail.com>
2026-08-07 22:11:24 +0200

    We are getting closer
    Just one small problem with the food, but this is going great

--- FirstAIProject.py
+++ FirstAIProject.py
@@ -9,12 +9,8 @@ grid = [
 
 #Creature
 health = 100
-hunger = 80
-energy = 60
-
-healthScore = 100 - health
-hungerScore = 100 - hunger
-energyScore = 100 - energy
+hunger = 100
+energy = 100
 
 creaturey = 2
 creaturex = 2
@@ -28,34 +24,69 @@ foodx = 4
 grid[foody][foodx] = "F"
 
 #AI
-#Movement
-distance = abs(creaturex - foodx) + abs(creaturey - foody)
+while health > 0 and hunger > 0 and energy > 0:
+    
+    #Decision making
+    healthScore = 100 - health
+    hungerScore = 100 - hunger
+    energyScore = 100 - energy
+    
+    #Old position
+    oldx = creaturex
+    oldy = creaturey
 
-#Decision making
-if hungerScore > energyScore:
-    print("Creature wants to eat")
-    if creaturex < foodx:
-        creaturex += 1
-        hunger -= 10
-        energy -= 5
-    elif creaturex > foodx:
-        creaturex -= 1
-        hunger -= 10
-        energy -= 5
-    elif creaturey < foody:
-        creaturey += 1
-        hunger -= 10
-        energy -= 5
-    elif creaturey > foody:
-        creaturey -= 1
-        hunger -= 10
-        energy -= 5
-elif energyScore > hungerScore:
-    print("Creature wants to sleep")
-    energy += 10
-else:
-    print("Creature is content")
+    #Decision making
+    if hungerScore > energyScore:
+        print("Creature wants to eat")
+        
+        #Movement
+        if creaturex < foodx:
+            creaturex += 1
+            hunger = max(hunger - 10, 0)
+            energy = max(energy - 5, 0)
+        elif creaturex > foodx:
+            creaturex -= 1
+            hunger = max(hunger - 10, 0)
+            energy = max(energy - 5, 0)
+        elif creaturey < foody:
+            creaturey += 1
+            hunger = max(hunger - 10, 0)
+            energy = max(energy - 5, 0)
+        elif creaturey > foody:
+            creaturey -= 1
+            hunger = max(hunger - 10, 0)
+            energy = max(energy - 5, 0)
+            
+        if creaturex == foodx and creaturey == foody:
+            hunger = min(hunger + food, 100)
+            print("Creature has eaten the food")
+            
+    elif energyScore > hungerScore:
+        print("Creature wants to sleep")
+        energy = min(energy + 10, 100)
+        print("Creature has slept")
+        
+    else:
+        hunger = max(hunger - 5, 0)
+        print("Creature is content")
+        
+    #Grid update
+    grid[oldy][oldx] = "."
+    grid[creaturey][creaturex] = "C"
+    
+    #Grid Food
+    grid[foody][foodx] = "F"
 
-#The thing idk
-for row in grid:
-    print(" ".join(row))
+    #The thing idk
+    print("Health:", health)
+    print("Hunger:", hunger)
+    print("Energy:", energy)
+    print()
+    
+    for row in grid:
+        print(" ".join(row))
+        
+    print()
+    input("Press Enter to continue...")
+        
+print("Creature has died")
